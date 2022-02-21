f75d2fe6a00aeb70d9265f4643bb5cc4184e1407
ByMikiii <mikixr1@gmail.com>
2022-02-21 22:33:36 +0100

    fkasjkjfshajk

--- Todo/JS/todo-script.js
+++ Todo/JS/todo-script.js
@@ -1,10 +1,10 @@
 let i = 1;
 function pushButton() {
-  if (i === 1) {
-    document.getElementById("todo1").style.textDecoration = "line-through";
+  if (i == 1) {
+    document.getElementById("btn1").style.textDecoration = "line-through";
     i++;
   } else {
-    document.getElementById("todo1").style.textDecoration = "none";
+    document.getElementById("btn1").style.textDecoration = "none";
     i = 1;
   }
   console.log(i);
